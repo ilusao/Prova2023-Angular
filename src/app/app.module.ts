460fed1e438142c5ab423bb41de6f697235833ec
André bernardes <andreapps.com@gmail.com>
2023-06-20 08:46:45 -0300

    1 etapa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProvaangularComponent } from './provaangular/provaangular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProvaangularComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/provaangular/provaangular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provaangular',
+  templateUrl: './provaangular.component.html',
+  styleUrls: ['./provaangular.component.css']
+})
+export class ProvaangularComponent {
+
+}
